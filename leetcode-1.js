c7cfb10cdf67f161f546b4967902c3e5b8c23d0e
Hermanda Mak <hermandamak@gmail.com>
2022-08-23 11:33:53 -0700

    add one problem

--- leetcode-1.js
+++ leetcode-1.js
@@ -102,3 +102,36 @@ var findMiddleIndex = function(nums) {
   }
   return -1;
 };
+
+/*
+You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the integer. 
+The digits are ordered from most significant to least significant in left-to-right order. The large integer does not contain any leading 0's.
+Increment the large integer by one and return the resulting array of digits.
+
+Constraints:
+1 <= digits.length <= 100
+0 <= digits[i] <= 9
+digits does not contain any leading 0's.
+
+@param {number[]} digits
+@return {number[]}
+*/
+
+var plusOne = function (digits) {
+  digits[digits.length - 1]++;
+  if (digits[digits.length - 1] === 10) {
+    for (let i = digits.length - 1; i > 0; i--) {
+      if (digits[i] === 10) {
+        digits[i] = 0;
+        digits[i - 1]++;
+      } else {
+        break;
+      }
+    }
+  }
+  if (digits[0] === 10) {
+    digits[0] = 0;
+    digits.unshift(1);
+  }
+  return digits;
+};

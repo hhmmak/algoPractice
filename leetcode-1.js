8c4b815e3aff33558885e556d884eac39b003147
Hermanda Mak <hermandamak@gmail.com>
2022-08-23 18:27:50 -0700

    add one problem

--- leetcode-1.js
+++ leetcode-1.js
@@ -135,3 +135,50 @@ var plusOne = function (digits) {
   }
   return digits;
 };
+/*
+Given an m x n matrix mat, return an array of all the elements of the array in a diagonal order.
+Input: mat = [[1,2,3],[4,5,6],[7,8,9]]
+Output: [1,2,4,7,5,3,6,8,9]
+
+Constraints:
+m == mat.length
+n == mat[i].length
+1 <= m, n <= 104
+1 <= m * n <= 104
+-105 <= mat[i][j] <= 105
+
+@param {number[][]} mat
+@return {number[]}
+*/
+
+var findDiagonalOrder = function(mat) {
+  let arr = [];
+  let i = 0;
+  let j = 0;
+  let m = mat[0].length - 1;
+  let n = mat.length - 1;
+  while(i !== m || j !== n){
+      arr.push(mat[j][i]);
+      if ((i + j) % 2) {      // goes down
+          if(j === n){
+              i ++;
+          } else if (i === 0){
+              j ++;
+          } else {
+              i --;
+              j ++;
+          }
+      } else {        //goes up
+          if(i === m){
+              j ++;
+          } else if (j === 0){
+              i ++;
+          } else {
+              i ++;
+              j --;
+          }
+      } 
+  }
+  arr.push(mat[j][i]);
+  return arr;
+};

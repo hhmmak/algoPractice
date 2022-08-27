17aec26f7da0a3bcebf470433bbc72e0213cffad
Hermanda Mak <hermandamak@gmail.com>
2022-08-26 22:45:25 -0700

    add one problem

--- leetcode-1.js
+++ leetcode-1.js
@@ -332,3 +332,41 @@ var strStr = function (haystack,needle) {
   }
   return -1;
 };
+
+
+/*
+Minimum Size Subarray Sum
+Given an array of positive integers nums and a positive integer target, 
+return the minimal length of a contiguous subarray [numsl, numsl+1, ..., numsr-1, numsr] of which the sum is greater than or equal to target. 
+If there is no such subarray, return 0 instead.
+
+Constraints:
+
+1 <= target <= 109
+1 <= nums.length <= 105
+1 <= nums[i] <= 104
+
+
+*/
+var minSubArrayLen = function(target, nums) {
+  let sum = 0;
+  let left = 0;
+  let amount = nums.length + 1;
+  for( let i = 0; i < nums.length; i++){
+    sum += nums[i];
+    while (sum >= target){
+      if (amount > (i - left + 1)){
+        amount = i - left + 1;
+      }
+      sum -= nums[left];
+      left ++;
+    }
+  }
+  
+  if (amount > nums.length){
+    return 0;
+  }
+  
+  return amount; 
+}
+  

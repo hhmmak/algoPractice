1f9c9c78d6b6394af570854d3f0e9cb3dd3e6d60
Hermanda Mak <hermandamak@gmail.com>
2022-08-29 20:04:33 -0700

    add one problem

--- leetcode-hash.js
+++ leetcode-hash.js
@@ -79,3 +79,39 @@ MyHashMap.prototype.remove = function(key) {
   this.map[key] = -1;
 };
 
+/*
+Single Number
+Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+You must implement a solution with a linear runtime complexity and use only constant extra space.
+
+Constraints:
+1 <= nums.length <= 3 * 104
+-3 * 104 <= nums[i] <= 3 * 104
+Each element in the array appears twice except for one element which appears only once.
+
+@param {number[]} nums
+@return {number}
+*/
+
+var singleNumber = function(nums) {
+  // nums.sort((a,b) => a - b);
+  // for (let i = 0; i < nums.length; i += 2){
+  //   if (nums[i] !== nums[i + 1]){
+  //     return nums[i];
+  //   }
+  // }
+  
+  let map = {};
+  for (let value of nums){
+    if (map[value]) {
+      map[value] = false;
+    } else {
+      map[value] = true
+    }
+  }
+  for (let value of nums) {
+    if (map[value]){
+      return value;
+    }
+  }
+};

421242760275a8d84a670bb18f21ad876de07fcb
Hermanda Mak <hermandamak@gmail.com>
2022-08-29 21:27:12 -0700

    add one problem

--- leetcode-hash.js
+++ leetcode-hash.js
@@ -153,3 +153,37 @@ var intersection = function(nums1, nums2) {
   }
   return arr;
 };
+
+/*
+Happy Number
+Write an algorithm to determine if a number n is happy.
+A happy number is a number defined by the following process:
+- Starting with any positive integer, replace the number by the sum of the squares of its digits.
+- Repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1.
+- Those numbers for which this process ends in 1 are happy.
+Return true if n is a happy number, and false if not.
+
+Constraints:
+1 <= n <= 231 - 1
+
+@param {number} n
+@return {boolean}
+*/
+
+// hash method
+var isHappy = function(n) {
+  let map = {};
+  while (n !== 1){
+    let arr = n.toString().split('');
+    n = arr.reduce((a,b) => a + b * b, 0)
+
+    //detect repeat presence of sum of digit, i.e. entered a loop since sum of digit will not change
+    if (n < 100){
+      if (map[n]){
+        return false;
+      }
+      map[n] = true;
+    }
+  }
+  return true;
+};

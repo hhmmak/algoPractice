de810b0138cdb67d0099235c338705a869dcba95
Hermanda Mak <hermandamak@gmail.com>
2022-08-30 01:08:56 -0700

    add one problem

--- leetcode-hash.js
+++ leetcode-hash.js
@@ -187,3 +187,31 @@ var isHappy = function(n) {
   }
   return true;
 };
+
+/*
+Two Sum
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+You can return the answer in any order.
+
+Constraints:
+2 <= nums.length <= 104
+-109 <= nums[i] <= 109
+-109 <= target <= 109
+Only one valid answer exists.
+
+@param {number[]} nums
+@param {number} target
+@return {number[]}
+*/
+
+var twoSum = function(nums, target) {
+  let map = {};
+  for (let i = 0; i < nums.length; i++) {
+    if (isNaN(map[nums[i]])) {
+      map[target - nums[i]] = i;
+    } else {
+      return [ map[nums[i]], i ];
+    }
+  }
+};

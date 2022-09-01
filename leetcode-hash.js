bac01dbfc8e9328bf3740dc55c647aa77d7ecadb
Hermanda Mak <hermandamak@gmail.com>
2022-08-31 23:04:28 -0700

    add one problem

--- leetcode-hash.js
+++ leetcode-hash.js
@@ -476,3 +476,81 @@ var groupAnagrams = function(strs) {
   
   return Object.values(map);
 };
+
+/*
+Valid Sudoku
+Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+Each row must contain the digits 1-9 without repetition.
+Each column must contain the digits 1-9 without repetition.
+Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+
+Note:
+A Sudoku board (partially filled) could be valid but is not necessarily solvable.
+Only the filled cells need to be validated according to the mentioned rules.
+
+Example:
+Input: board = 
+[["5","3",".",".","7",".",".",".","."]
+,["6",".",".","1","9","5",".",".","."]
+,[".","9","8",".",".",".",".","6","."]
+,["8",".",".",".","6",".",".",".","3"]
+,["4",".",".","8",".","3",".",".","1"]
+,["7",".",".",".","2",".",".",".","6"]
+,[".","6",".",".",".",".","2","8","."]
+,[".",".",".","4","1","9",".",".","5"]
+,[".",".",".",".","8",".",".","7","9"]]
+Output: true
+
+Constraints:
+board.length == 9
+board[i].length == 9
+board[i][j] is a digit 1-9 or '.'.
+
+@param {character[][]} board
+@return {boolean}
+*/
+
+var isValidSudoku = function(board) {
+  let square = {}; // map for 3*3 grid check
+  for (let i = 0; i < 9; i ++) {
+
+    //reset upon new line
+    let row = {};
+    let column = {};
+
+    for (let j = 0; j < 9; j++){
+
+      //set up new map for each 3*3 grid
+      //can be replaced by for loop i = 0 to 8 where square[i] = {}
+      if (!(i % 3 || j % 3)) {
+        square[Math.floor(i / 3) * 3 + Math.floor(j / 3)] = {};
+      }
+
+      // check row & all grids
+      if (board[i][j] !== '.') {
+        if (row[board[i][j]]){
+          return false
+        } else {
+          row[board[i][j]] = 1;
+        }
+
+        //check grid
+        if (square[Math.floor(i / 3) * 3 + Math.floor(j / 3)][board[i][j]]) {
+          return false; 
+        } else {
+          square[Math.floor(i / 3) * 3 + Math.floor(j / 3)][board[i][j]] = 1;
+        }
+      }
+
+      // check column
+      if (board[j][i] !== '.') {
+        if (column[board[j][i]]){
+          return false;
+        } else {
+          column[board[j][i]] = 1;
+        }
+      }
+    }
+  }
+  return true;
+}

dda217a9c11e8768ec291595d2862780e0f6604e
Hermanda Mak <hermandamak@gmail.com>
2022-08-31 19:01:01 -0700

    add one problem

--- leetcode-hash.js
+++ leetcode-hash.js
@@ -448,3 +448,31 @@ var findRestaurant = function(list1, list2) {
   return common;
   
 };
+
+/*
+Group Anagrams
+Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+Constraints:
+1 <= strs.length <= 104
+0 <= strs[i].length <= 100
+strs[i] consists of lowercase English letters.
+
+@param {string[]} strs
+@return {string[][]}
+*/
+
+var groupAnagrams = function(strs) {
+  let map = {};  
+  for (let i = 0; i < strs.length; i++){
+      let compare = strs[i].split('').sort().join('');
+      if (map[compare]){
+        map[compare].push(strs[i]);
+      } else {
+        map[compare] = [strs[i]];
+      }
+  }
+  
+  return Object.values(map);
+};

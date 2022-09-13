e895a5fe811572882049e14cea816a18abcc6d14
Hermanda Mak <hermandamak@gmail.com>
2022-09-13 16:06:22 -0700

    add one problem

--- leetcode-linkedlist.js
+++ leetcode-linkedlist.js
@@ -467,3 +467,54 @@ var addTwoNumbers = function(l1, l2) {
   
   return head;
 };
+
+/*
+Flatten a Multilevel Doubly Linked List
+You are given a doubly linked list, which contains nodes that have a next pointer, a previous pointer, and an additional child pointer. 
+This child pointer may or may not point to a separate doubly linked list, also containing these special nodes. 
+These child lists may have one or more children of their own, and so on, to produce a multilevel data structure as shown in the example below.
+Given the head of the first level of the list, flatten the list so that all the nodes appear in a single-level, doubly linked list. 
+Let curr be a node with a child list. The nodes in the child list should appear after curr and before curr.next in the flattened list.
+Return the head of the flattened list. The nodes in the list must have all of their child pointers set to null.
+
+Constraints:
+The number of Nodes will not exceed 1000.
+1 <= Node.val <= 105
+*/
+
+var flatten = function(head) {
+  
+  if (!head){
+    return head;
+  }
+  
+  const read = (pt) => {
+    let next = pt;
+    while (next){
+      if (pt.child){
+        let remain = pt.next;
+        pt.next = pt.child;
+        pt.child.prev = pt;
+        pt.child = null;
+        if (remain){
+          remain.prev = read(pt.next);
+          remain.prev.next = remain;
+          next = remain;
+        }
+      } else {
+        next = pt.next;
+      }
+      if (next){
+        pt = next;
+      }
+    }
+    return pt;
+  }
+  
+  let node = head;
+  read(node);
+  
+  return node;
+
+
+};

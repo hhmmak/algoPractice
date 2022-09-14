576f293ba09586bda29eb45e5ca3c95a659064d3
Hermanda Mak <hermandamak@gmail.com>
2022-09-13 22:48:21 -0700

    add one problem

---
+++ leetcode-recursion.js
@@ -0,0 +1,17 @@
+/*
+Search in a Binary Search Tree
+*/
+
+var searchBST = function(root, val) {
+  if (root === null){
+    return null;
+	}
+	if (root.val === val){
+		return root;
+	}
+  if (root.val < val){
+    return searchBST(root.right, val);
+  }
+  //if root.val >= val
+	return searchBST(root.left, val);
+};

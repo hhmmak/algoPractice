65b497692b48bdf835dce6a4870d87ae6551ecad
Hermanda Mak <hermandamak@gmail.com>
2022-09-14 22:17:35 -0700

    edit one problem

--- leetcode-recursion.js
+++ leetcode-recursion.js
@@ -19,6 +19,12 @@ var searchBST = function(root, val) {
 /*
 Fibonacci Number
 Constraints: 0 <= n <= 30
+
+Climbing Stairs
+You are climbing a staircase. It takes n steps to reach the top.
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+Constraints: 1 <= n <= 45
 */
 
 //map + recursion(memoization); time 2nd space 2nd

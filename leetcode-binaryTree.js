086803c5badc3c60722d38c859a8dc6b0e8f7dc1
Hermanda Mak <hermandamak@gmail.com>
2022-09-25 23:30:07 -0700

    add one problem

--- leetcode-binaryTree.js
+++ leetcode-binaryTree.js
@@ -266,3 +266,37 @@ var buildTree = function(preorder, inorder) {
 	return build(0, preorder.length - 1);
 
 };
+
+/*
+Populating Next Right Pointers in Each Node
+You are given a perfect binary tree where all leaves are on the same level, and every parent has two children. 
+Populate each next pointer to point to its next right node. If there is no next right node, the next pointer should be set to NULL.
+Initially, all next pointers are set to NULL.
+You may only use constant extra space.
+The recursive approach is fine. You may assume implicit stack space does not count as extra space for this problem.
+
+Constraints:
+The number of nodes in the tree is in the range [0, 212 - 1].
+-1000 <= Node.val <= 1000
+*/
+
+var connect = function(root) {  
+  let prevHead = root;
+
+	while (prevHead && prevHead.left) {
+		let prev = prevHead;
+		let node = prev.left;
+		while  (!(prev.next === null && node === prev.right)) {
+			if (node === prev.right) {
+        prev = prev.next;
+				node.next = prev.left;
+			} else {
+				node.next = prev.right;
+			}
+			node = node.next;
+		}
+		prevHead = prevHead.left;
+	}
+	return root;
+
+};

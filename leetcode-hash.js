ed88201dba79c965babb82217f3a0179b171d9b1
Hermanda Mak <hermandamak@gmail.com>
2022-10-05 00:31:34 -0700

    add one problem

--- leetcode-hash.js
+++ leetcode-hash.js
@@ -811,3 +811,40 @@ RandomizedSet.prototype.remove = function(val) {
 RandomizedSet.prototype.getRandom = function() {
 return this.set[Math.floor(Math.random()*(this.set.length))];
 };
+
+/*
+Task Scheduler
+Given a characters array tasks, representing the tasks a CPU needs to do, where each letter represents a different task. Tasks could be done in any order. 
+Each task is done in one unit of time. For each unit of time, the CPU could complete either one task or just be idle.
+However, there is a non-negative integer n that represents the cooldown period between two same tasks (the same letter in the array), 
+that is that there must be at least n units of time between any two same tasks.
+
+Return the least number of units of times that the CPU will take to finish all the given tasks.
+*/
+
+var leastInterval = function(tasks, n) {
+  if (tasks.length === 1){
+    return 1;
+  }
+  if (n === 0){
+    return tasks.length;
+  }
+  let maxRep = 0;
+  let map = {};
+  for (let i = 0; i < tasks.length; i++){
+    map[tasks[i]] = (map[tasks[i]] || 0) + 1;
+    if (map[tasks[i]] > maxRep){
+      maxRep = map[tasks[i]];
+    }
+  }
+  
+  let maxTask = 0;
+  for (let key in map){
+    if (map[key] === maxRep){
+      maxTask ++;
+    }
+  }
+
+  let minUnit = (maxRep - 1) * (n + 1) + maxTask;
+  return Math.max(minUnit, tasks.length);
+};

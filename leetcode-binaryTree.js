0ba3a1ec3fc8a91948526e61c9e8e358b1327bc1
Hermanda Mak <hermandamak@gmail.com>
2022-10-09 20:29:14 -0700

    add one problem

--- leetcode-binaryTree.js
+++ leetcode-binaryTree.js
@@ -451,3 +451,30 @@ var isBalanced = function(root) {
   return check(root) !== -1;
   
 };
+
+/*
+Diameter of Binary Tree
+Given the root of a binary tree, return the length of the diameter of the tree.
+The diameter of a binary tree is the length of the longest path between any two nodes in a tree. This path may or may not pass through the root.
+The length of a path between two nodes is represented by the number of edges between them.
+*/
+var diameterOfBinaryTree = function(root) {
+    
+  let maxLength = 0;
+
+  const check = (node) => {
+    if (!node){
+      return 0;
+    }
+    
+    let left = check(node.left);
+    let right = check(node.right);
+    if (left + right > maxLength){
+      maxLength = left + right;
+    }
+    return Math.max(left, right) + 1;
+  }
+  
+  check(root);
+  return maxLength;
+};

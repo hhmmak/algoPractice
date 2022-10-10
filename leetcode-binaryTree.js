49218b297cdaea08b9a859799932a92ae631b209
Hermanda Mak <hermandamak@gmail.com>
2022-10-09 18:33:06 -0700

    add one problem

--- leetcode-binaryTree.js
+++ leetcode-binaryTree.js
@@ -418,3 +418,36 @@ var invertTree = function(root) {
 
   return root;
 };
+
+/*
+Balanced Binary Tree
+Given a binary tree, determine if it is height-balanced.
+*/
+
+var isBalanced = function(root) {
+  
+  if (!root || (!root.left && !root.right)) {
+    return true;
+  }
+
+  let check = (node) => {
+    if (!node) {
+      return 0;
+    }
+    let left = check(node.left);
+    if (left === -1){
+      return -1;
+    }
+    let right = check(node.right);
+    if (right === -1){
+      return -1
+    }
+    if (Math.abs(left - right) > 1){
+      return -1;
+    }
+    return Math.max(left, right) + 1;
+  }
+  
+  return check(root) !== -1;
+  
+};

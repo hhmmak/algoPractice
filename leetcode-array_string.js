38526f10e2ca0cf325bd120ff5fbbe46270e191c
Hermanda Mak <hermandamak@gmail.com>
2022-10-20 14:05:36 -0700

    edit one problem

--- leetcode-array_string.js
+++ leetcode-array_string.js
@@ -802,7 +802,7 @@ var findMin = function(nums) {
 
 /*
 3Sum
-Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0 (no duplicates).
+Given an integer array nums, return all the unique triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
 */
 
 var threeSum = function(nums) {

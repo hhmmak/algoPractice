ba395fe60141eb32fbae83686b2bd7ccabd6769b
Hermanda Mak <hermandamak@gmail.com>
2022-10-19 17:21:41 -0700

    add one problem

--- leetcode-array_string.js
+++ leetcode-array_string.js
@@ -729,3 +729,26 @@ var productExceptSelf = function(nums) {
   
   return result;
 };
+
+/*
+Maximum Subarray
+Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+A subarray is a contiguous part of an array.
+*/
+
+var maxSubArray = function(nums) {
+  if (nums.length === 1){
+    return nums[0];
+  }
+  let length = nums.length;
+  let max = nums[0];
+  let prev = nums[0];
+  
+  for (let i = 1; i < length; i++){
+    let sum = (prev > 0 ? prev : 0) + nums[i];
+    max = Math.max(max, sum);
+    prev = sum;
+  }
+  
+  return max;
+};

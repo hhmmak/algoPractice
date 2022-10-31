ced81dde24d7e4e7bb6602a7bcce9f644c00cdc7
Hermanda Mak <hermandamak@gmail.com>
2022-10-31 14:23:55 -0700

    add one problem

--- leetcode-array_string.js
+++ leetcode-array_string.js
@@ -1041,3 +1041,29 @@ var longestPalindrome = function(s) {
   }
   return substring;
 };
+
+/*
+Best Time to Buy and Sell Stock II
+You are given an integer array prices where prices[i] is the price of a given stock on the ith day.
+On each day, you may decide to buy and/or sell the stock. You can only hold at most one 
+share of the stock at any time. However, you can buy it then immediately sell it on the same day.
+Find and return the maximum profit you can achieve.
+
+
+*/
+
+var maxProfit = function(prices) {
+  let buy = 0;
+  let sell = 0;
+  let profit = 0;
+  
+  while (sell < prices.length){
+    if (sell + 1 >= prices.length || prices[sell + 1] < prices[sell]){
+      profit += (prices[sell] - prices[buy]);
+      buy = sell + 1;
+    }
+    sell ++;
+  }
+
+  return profit;
+};

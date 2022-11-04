1c81193b78f72747019cf17ea256606a3090d0e3
Hermanda Mak <hermandamak@gmail.com>
2022-11-03 17:36:34 -0700

    add one problem

--- leetcode-array_string.js
+++ leetcode-array_string.js
@@ -1295,3 +1295,27 @@ var uniquePaths = function(m, n) {
   }
   return walk(0, 0);
 };
+
+/*
+Jump Game
+You are given an integer array nums. You are initially positioned at the array's first index, and each element in the array represents your 
+maximum jump length at that position.
+Return true if you can reach the last index, or false otherwise.
+*/
+
+var canJump = function(nums) {
+  let map = new Array(nums.length);
+  map[nums.length - 1] = true;
+  let remain = 1;
+  for (let i = 0; i < nums.length - 1; i ++){
+    if (nums[i] > nums.length - i) {
+      return true;
+    }
+    remain --;
+    remain = Math.max(remain, nums[i]);
+    if (remain < 1){
+      return false;
+    }
+  }
+  return true;
+};

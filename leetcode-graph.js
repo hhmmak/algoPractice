cca2728eadf4008ed089bfa4018cdc61eead763a
Hermanda Mak <hermandamak@gmail.com>
2022-12-20 15:11:44 -0800

    add one problem

--- leetcode-graph.js
+++ leetcode-graph.js
@@ -46,3 +46,44 @@ var validPath = function(n, edges, source, destination) {
   
   return false;
 };
+
+/*
+Keys and Rooms
+
+There are n rooms labeled from 0 to n - 1 and all the rooms are locked except for room 0. Your goal is to visit all the rooms. 
+However, you cannot enter a locked room without having its key.
+
+When you visit a room, you may find a set of distinct keys in it. Each key has a number on it, denoting which room it unlocks, 
+and you can take all of them with you to unlock the other rooms.
+
+Given an array rooms where rooms[i] is the set of keys that you can obtain if you visited room i, 
+return true if you can visit all the rooms, or false otherwise.
+
+*/
+
+/**
+ * @param {number[][]} rooms
+ * @return {boolean}
+ */
+
+//BFS, graph
+// Space complexity: O(n), n = rooms.length;
+// Time complexity: O(2n + k), n = rooms.length, k = total number of keys = total pieces of integers in rooms array
+var canVisitAllRooms = function(rooms) {
+  let visited = new Array(rooms.length);
+  visited.fill(false);
+  visited[0] = true;
+
+  let queue = rooms[0];
+  while (queue.length > 0){
+    let key = queue.shift();
+    visited[key] = true;
+    for (let i = 0; i < rooms[key].length; i++){
+      if (visited[rooms[key][i]] === false){
+        queue.push(rooms[key][i])
+      }
+    }
+  }
+
+  return visited.every(ele => ele);
+};

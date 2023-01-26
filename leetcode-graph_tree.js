b942fcb685831b7a14066e1248ba09a77f162f4d
Hermanda Mak <hermandamak@gmail.com>
2023-01-25 17:09:16 -0800

    add one problem

--- leetcode-graph_tree.js
+++ leetcode-graph_tree.js
@@ -169,3 +169,48 @@ var countSubTrees = function(n, edges, labels) {
   return ans;
 }
 
+/*
+Find Closest Node to Given Two Nodes
+You are given a directed graph of n nodes numbered from 0 to n - 1, where each node has at most one outgoing edge.
+
+The graph is represented with a given 0-indexed array edges of size n, indicating that there is a directed edge 
+from node i to node edges[i]. If there is no outgoing edge from i, then edges[i] == -1.
+
+You are also given two integers node1 and node2.
+
+Return the index of the node that can be reached from both node1 and node2, such that the maximum between 
+the distance from node1 to that node, and from node2 to that node is minimized. If there are multiple answers, return the node 
+with the smallest index, and if no possible answer exists, return -1.
+
+Note that edges may contain cycles.
+*/
+
+
+var closestMeetingNode = function(edges, node1, node2) {
+
+  const find = (node1, node2, visited1, visited2) => {
+    if (visited2[node1] && visited1[node2] && node1 !== -1 && node2 !== -1) {
+      return Math.min(node1, node2);
+    }
+    if (visited2[node1] && node1 !== -1) return node1;
+    if (visited1[node2] && node2 !== -1) return node2;
+    if (node1 === node2) return node1;
+    if (visited1[node1] && visited2[node2]) return -1;
+    
+    visited1[node1] = true;
+    visited2[node2] = true;
+
+    let next1 = edges[node1];
+    let next2 = edges[node2];
+    if (node1 === -1) next1 = -1;
+    if (node2 === -1) next2 = -1;
+
+    return find(next1, next2, visited1, visited2);
+  }
+  
+
+  let result = find(node1, node2, {}, {});
+
+
+  return result;
+};

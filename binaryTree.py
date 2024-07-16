1eee70b3f6a6054d04c2e947116536a6a3fbadf3
Hermanda Mak <hermandamak@gmail.com>
2024-07-16 15:50:10 -0700

    update

--- binaryTree.py
+++ binaryTree.py
@@ -6,12 +6,12 @@ class TreeNode(object):
         self.right = right
 
 
-"""
-2196. Create Binary Tree From Descriptions
-
-"""
-
 class Solution(object):
+
+    """
+    2196. Create Binary Tree From Descriptions
+    """
+
     def createBinaryTree(self, descriptions):
         """
         :type descriptions: List[List[int]]
@@ -51,3 +51,95 @@ class Solution(object):
                 break
         
         return root
+
+
+    """ 
+    Return array/list of values of node in Shortest Route From a Binary Tree Node to another
+    """
+
+    def getDirections(self, root, startValue, destValue):
+        """
+        :type root: Optional[TreeNode]
+        :type startValue: int
+        :type destValue: int
+        :rtype: str
+        """
+
+        def dfs(node, val):
+            if node is None:
+                return None
+            
+            # found target value
+            if node.val == val:
+                return [node.val]
+            
+            # continue dfs
+            left = dfs(node.left, val)
+            right = dfs(node.right, val)
+
+            route = left if left else right
+            if route:
+                route.append(node.val)
+                print(route)
+            
+            return route
+        
+        root_to_dest = dfs(root, destValue)
+        root_to_start = dfs(root, startValue)
+
+        start_idx = len(root_to_start) - 1
+        parent_val = root.val
+        while len(root_to_dest) > 0 and len(root_to_start) > 0 and root_to_dest[-1] == root_to_start[-1]:
+            root_to_dest.pop()
+            parent_val = root_to_start.pop()
+
+        return root_to_start + [parent_val] + root_to_dest[::-1]
+
+"""
+2096. Step-By-Step Directions From a Binary Tree Node to Another
+"""
+
+
+def getDirections(self, root, startValue, destValue):
+        """
+        :type root: Optional[TreeNode]
+        :type startValue: int
+        :type destValue: int
+        :rtype: str
+        """
+
+        # get direction from root to targetValue
+        def dfs(node, val):
+            if node is None:
+                return "" 
+            if node.val == val:
+                return "E"
+            
+            # continue dfs
+            left = dfs(node.left, val)
+            if left:
+                return "L" + left
+
+            right = dfs(node.right, val)            
+            if right:
+                return "R" + right
+            
+            # return case for if target is root
+            return ""
+        
+        root_to_start = dfs(root, startValue)[:-1]
+        
+        root_to_dest = dfs(root, destValue)[:-1]
+
+        # find duplicate routes
+        idx = 0
+        while idx < len(root_to_start) and idx < len(root_to_dest) \
+                and root_to_dest[idx] == root_to_start[idx]:
+            print(idx)
+            idx += 1
+        
+        # create direction from startValue to root
+        start_to_root = "U" * (len(root_to_start) - idx )
+
+        # combine directions
+        return start_to_root + root_to_dest[idx:]

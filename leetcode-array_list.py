36a9c70c676e66e6bab316aeb808875b74f8f5b5
Hermanda Mak <hermandamak@gmail.com>
2025-02-13 19:26:06 -0800

    add a problem

--- leetcode-array_list.py
+++ leetcode-array_list.py
@@ -1,7 +1,40 @@
 import math
 from typing import List
 
+"""
+1352. Product of the Last K Numbers
+"""
+class ProductOfNumbers(object):
 
+    def __init__(self):
+        self.product = [1]
+
+    def add(self, num):
+        """
+        :type num: int
+        :rtype: None
+        """
+        if num == 0:
+            self.product = [1]
+        else:
+            self.product.append(num * self.product[-1])
+        
+
+    def getProduct(self, k):
+        """
+        :type k: int
+        :rtype: int
+        """
+        if k >= len(self.product):
+            return 0
+        return self.product[-1] / self.product[-k-1]
+        
+
+
+# Your ProductOfNumbers object will be instantiated and called as such:
+# obj = ProductOfNumbers()
+# obj.add(num)
+# param_2 = obj.getProduct(k)
 class Solution:
 
     """
